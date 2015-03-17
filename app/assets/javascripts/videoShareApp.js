6e7575291d66622534a9027c1d7bf3afb4250c3b
Tao Feng <taofeng311@gmail.com>
2015-03-17 15:32:37 +0000

    Change to Heroku URL

--- app/assets/javascripts/videoShareApp.js
+++ app/assets/javascripts/videoShareApp.js
@@ -5,12 +5,12 @@ app.controller('videoShareController', function($scope, $http) {
     $scope.reverseSort = true;
     $scope.currentPage = 1;
     $scope.pages = 0;
-    $http.get("https://rails-tutorial-bailei.c9.io/videos").success(function(response){
+    $http.get("http://salty-wave-2170.herokuapp.com/videos").success(function(response){
         $scope.pages = Math.ceil(response.length / 6);
     });
 
     $scope.loadPage = function(){
-      $http.get("https://rails-tutorial-bailei.c9.io/videos?page=" + $scope.currentPage).success(function(response){
+      $http.get("http://salty-wave-2170.herokuapp.com/videos?page=" + $scope.currentPage).success(function(response){
         $scope.videos = response;
       });
     };
