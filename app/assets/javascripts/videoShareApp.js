e1186506904b9fde4d943b121e6edcbe26a6f44a
Tao Feng <taofeng311@gmail.com>
2015-03-17 16:03:28 +0000

    fix bug on localhost

--- app/assets/javascripts/videoShareApp.js
+++ app/assets/javascripts/videoShareApp.js
@@ -3,12 +3,12 @@ angular.module("videoShareApp", []).controller('videoShareController', function(
     $scope.reverseSort = true;
     $scope.currentPage = 1;
     $scope.pages = 0;
-    $http.get("http://salty-wave-2170.herokuapp.com/videos").success(function(response){
+    $http.get("http://localhost:3000/videos").success(function(response){
         $scope.pages = Math.ceil(response.length / 6);
     });
 
     $scope.loadPage = function(){
-      $http.get("http://salty-wave-2170.herokuapp.com/videos?page=" + $scope.currentPage).success(function(response){
+      $http.get("http://localhost:3000/videos?page=" + $scope.currentPage).success(function(response){
         $scope.videos = response;
       });
     };
