010d5423ca486b838b9ee75404b3f1535121d86a
Tao Feng <taofeng311@gmail.com>
2015-03-18 18:39:08 +0000

    Change API url to heroku APP url

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require angular
+//= require angular-animate
+//= require angular-resource
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require angular
 //= require_tree .
--- app/assets/javascripts/videoShareApp.js
+++ app/assets/javascripts/videoShareApp.js
@@ -1,16 +1,17 @@
 angular.module("videoShareApp", []).controller('videoShareController', function($scope, $http) {
-    $scope.orderByField = 'firstName';
     $scope.reverseSort = true;
     $scope.currentPage = 1;
     $scope.pages = 0;
+    $http.get("http://salty-wave-2170.herokuapp.com/").success(function(response){
     // $http.get("https://rails-tutorial-bailei.c9.io/videos").success(function(response){
-    $http.get("http://localhost:3000/videos").success(function(response){
+    // $http.get("http://localhost:3000/videos").success(function(response){
         $scope.pages = Math.ceil(response.length / 6);
     });
 
     $scope.loadPage = function(){
+      $http.get("http://salty-wave-2170.herokuapp.com/?page=" + $scope.currentPage).success(function(response){
       // $http.get("https://rails-tutorial-bailei.c9.io/videos?page=" + $scope.currentPage).success(function(response){
-      $http.get("http://localhost:3000/videos?page=" + $scope.currentPage).success(function(response){
+      // $http.get("http://localhost:3000/videos?page=" + $scope.currentPage).success(function(response){
         $scope.videos = response;
       });
     };
